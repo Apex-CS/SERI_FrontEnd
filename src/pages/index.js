70866ce4718b79a1affd34b5aea69d0d69543b19
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-06 14:56:37 -0600

    indexing of pages

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Home from './Home/Home';
+import SignIn from './SignIn/SignIn';
+import LogIn from './LogIn/LogIn';
+import Movie from './Movie/Movie';
+import Movies from './Movies/Movies';
+import AddNewMovie from './FormMovie/AddNewMovie';
+
+
+export {
+    Home,
+    SignIn,
+    LogIn,
+    Movie,
+    Movies,
+    AddNewMovie,
+};
