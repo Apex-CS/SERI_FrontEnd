3b6f8bb4ca88bdef454eb2231dc1f7d6515eb48d
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-06 15:10:35 -0600

    add routing to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './styles/app.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Home, AddNewMovie, LogIn, Movie, Movies, SignIn} from './pages'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/addMovie' element={< AddNewMovie />}/>
+          <Route path='/login' element={<LogIn />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/movie' element={<Movie />} />
+          <Route path='/movies' element={<Movies />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
