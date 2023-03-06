3824483a3f3d0cd7fd8dd2400597feef55fda996
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-06 14:55:57 -0600

    indexing of components

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import MovieItem from "./Movie/MovieItem";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Navbar from "./Navbar/Navbar";
+import List from "./List/List";
+import NewsLetter from "./NewsLetter/NewLetter";
+import SignIn from "./SignIn/SignIn";
+import Comment from "./Comment/Comment";
+
+export {
+    MovieItem,
+    Footer,
+    Header,
+    Navbar,
+    List,
+    NewsLetter,
+    SignIn,
+    Comment,
+};
