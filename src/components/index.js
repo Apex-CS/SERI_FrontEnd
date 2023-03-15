486625343b045ee9473e3671bdf6c535786a3162
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-14 19:51:15 -0600

    Index new components

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,13 @@ import List from "./List/List";
 import NewsLetter from "./NewsLetter/NewLetter";
 import SignIn from "./SignIn/SignIn";
 import Comment from "./Comment/Comment";
+import Sidebar from "./Sidebar/Sidebar";
+import SignUp from "./SignUp/SignUp";
+import Carousel from "./Carousel/Carousel";
+import CommentBox from "./Comment/CommentBox";
+import InputText from "./InputText/InputText";
+import Search from "./Search/Search";
+import Loader from "./Loader/Loader";
 
 export {
     MovieItem,
@@ -16,4 +23,11 @@ export {
     NewsLetter,
     SignIn,
     Comment,
+    Sidebar,
+    SignUp,
+    Carousel,
+    CommentBox,
+    InputText,
+    Search,
+    Loader,
 };
