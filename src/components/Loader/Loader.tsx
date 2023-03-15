3ac65be3662210cd74b5ef41c9695b7f98683f53
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-14 19:46:56 -0600

    Loader component: Logic - Data - Class

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import './Loader.css';
+function Loader() {
+	return (
+		<div role='status'>
+            <span className="loader-icon sr-only "></span>
+            
+		</div>
+	);
+}
+
+export default Loader;
