316334325ff8d7a61eb6c602aa469ec8de176287
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-14 19:44:04 -0600

    List : Logic - Data - Class

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,5 +1,50 @@
-function List() {
-    return ( <>This is the List</> );
+import rottenTomatteLogo from "../../resources/img/list-rotten-tomatte.png";
+import { Movies } from "../../types/types";
+import Loader from "../Loader/Loader";
+interface ListProps {
+	title: string;
+	data: Movies[] | undefined;
+}
+
+function List({ title, data }: ListProps) {
+	console.log("🚀 ~ file: List.tsx:2 ~ List ~ data:", data);
+
+	return (
+		<div className='max-w-md ml-3 mt-3'>
+			<div className='flex justify-start items-center border-l-4 border-yellow-400'>
+				<h1 className='font-bold ml-1 dark:text-yellow-400'>{title}</h1>
+			</div>
+			<ul className='max-w-md divide-y '>
+				{data ? (
+					data.map((item, index) => (
+						<li
+							key={index}
+							className='pb-3 sm:pb-4 p-3 border-transparent	'>
+							<div className='flex space-x-4'>
+								<div className='flex-1 flex items-center min-w-0'>
+									<p className='w-auto flex justify-start text-sm font-medium text-gray-900 truncate dark:text-white'>
+										{item.title}
+									</p>
+								</div>
+								<div className='flex-shrink-0'>
+									<img
+										className='w-8 h-8 rounded-full'
+										src={rottenTomatteLogo}
+										alt='Neil image'
+									/>
+								</div>
+								<div className='inline-flex items-center text-base font-semibold text-gray-900 dark:text-white'>
+									Likes: {item.likes}
+								</div>
+							</div>
+						</li>
+					))
+				) : (
+					<Loader />
+				)}
+			</ul>
+		</div>
+	);
 }
 
 export default List;
