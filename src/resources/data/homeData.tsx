104ae32e8cd4f7272a6774c26c3c0d62157209d9
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-21 17:44:40 -0600

    add data

--- src/resources/data/homeData.tsx
+++ src/resources/data/homeData.tsx
@@ -1,27 +1,35 @@
 import { Movies } from "../../types/types";
+const MAX_NUMBER_ID = 99999999;
+const setIDMovie = () => {
+	return Math.round(Math.random() * MAX_NUMBER_ID);
+};
 
-const carrouselData = [
-	{ title: "A Man Called Otto", likes: Math.round(Math.random() * 100) },
+const carrouselData: Movies[] = [
+	{ title: "A Man Called Otto", likes: Math.round(Math.random() * 100), id: setIDMovie() },
 	{
 		title: "Everything Everywhere All at Once",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
-	{ title: "Missing", likes: Math.round(Math.random() * 100) },
+	{ title: "Missing", likes: Math.round(Math.random() * 100), id: setIDMovie() },
 	{
 		title: "Palm Trees and Power Lines",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
-	{ title: "The Whale", likes: Math.round(Math.random() * 100) },
+	{ title: "The Whale", likes: Math.round(Math.random() * 100), id: setIDMovie() },
 	{
 		title: "This Is Where I Leave You",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
-	{ title: "Triangle of Sadness", likes: Math.round(Math.random() * 100) },
-	{ title: "All That Breathes", likes: Math.round(Math.random() * 100) },
-	{ title: "A Man Called Otto", likes: Math.round(Math.random() * 100) },
+	{ title: "Triangle of Sadness", likes: Math.round(Math.random() * 100), id: setIDMovie() },
+	{ title: "All That Breathes", likes: Math.round(Math.random() * 100), id: setIDMovie() },
+	{ title: "A Man Called Otto", likes: Math.round(Math.random() * 100), id: setIDMovie() },
 	{
 		title: "The Banshees of Inisherin",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
 ];
 
@@ -29,27 +37,32 @@ const listDataTop10MoviesTemp: Movies[] = [
 	{
 		title: "Chris Rock: Selective Outrage",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
 	{
 		title: "Everything Everywhere All at Once",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
-	{ title: "Missing", likes: Math.round(Math.random() * 100) },
+	{ title: "Missing", likes: Math.round(Math.random() * 100), id: setIDMovie() },
 	{
 		title: "Palm Trees and Power Lines",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
-	{ title: "The Whale", likes: Math.round(Math.random() * 100) },
+	{ title: "The Whale", likes: Math.round(Math.random() * 100), id: setIDMovie() },
 	{
 		title: "This Is Where I Leave You",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
-	{ title: "Triangle of Sadness", likes: Math.round(Math.random() * 100) },
-	{ title: "All That Breathes", likes: Math.round(Math.random() * 100) },
-	{ title: "A Man Called Otto", likes: Math.round(Math.random() * 100) },
+	{ title: "Triangle of Sadness", likes: Math.round(Math.random() * 100), id: setIDMovie() },
+	{ title: "All That Breathes", likes: Math.round(Math.random() * 100), id: setIDMovie() },
+	{ title: "A Man Called Otto", likes: Math.round(Math.random() * 100), id: setIDMovie() },
 	{
 		title: "The Banshees of Inisherin",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
 ];
 
@@ -57,27 +70,32 @@ const listDataTop10ComingTheatersTemp: Movies[] = [
 	{
 		title: "Chris Rock: Selective Outrage",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
 	{
 		title: "Everything Everywhere All at Once",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
-	{ title: "Missing", likes: Math.round(Math.random() * 100) },
+	{ title: "Missing", likes: Math.round(Math.random() * 100), id: setIDMovie()},
 	{
 		title: "Palm Trees and Power Lines",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
-	{ title: "The Whale", likes: Math.round(Math.random() * 100) },
+	{ title: "The Whale", likes: Math.round(Math.random() * 100), id: setIDMovie()},
 	{
 		title: "This Is Where I Leave You",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
-	{ title: "Triangle of Sadness", likes: Math.round(Math.random() * 100) },
-	{ title: "All That Breathes", likes: Math.round(Math.random() * 100) },
-	{ title: "A Man Called Otto", likes: Math.round(Math.random() * 100) },
+	{ title: "Triangle of Sadness", likes: Math.round(Math.random() * 100), id: setIDMovie()},
+	{ title: "All That Breathes", likes: Math.round(Math.random() * 100), id: setIDMovie()},
+	{ title: "A Man Called Otto", likes: Math.round(Math.random() * 100), id: setIDMovie()},
 	{
 		title: "The Banshees of Inisherin",
 		likes: Math.round(Math.random() * 100),
+		id: setIDMovie(),
 	},
 ];
 
