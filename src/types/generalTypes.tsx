d5fb6069f99476d2dc731aab5d7e2f03da7af12b
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-21 17:45:09 -0600

    add types and info to /types/GeneralTypes

--- src/types/generalTypes.tsx
+++ src/types/generalTypes.tsx
@@ -1,24 +1,33 @@
+import React from "react";
+
 export type referenceLink = {
-    title: string,
-    href: string,
-}
+	title: string;
+	href: string;
+};
 
 export interface footerLinks extends referenceLink {
-    id?: number | string,
+	id?: number | string;
 }
 
 export interface footerSocialNetworkLinks extends referenceLink {
-    id?: number | string,
-    logo: string,
-
+	id?: number | string;
+	logo: string;
 }
 
 export interface HeaderLinks extends referenceLink {
-    id?: number | string,
+	id?: number | string;
 }
 
+export type InputTextStateProp = string | number;
+
+export enum InputTextTypeEnum {
+	email = "email",
+	text = "text",
+	password = "password",
+	tel = "tel",
+	number = "number",
+	url = "url",
+}
 // export interface footerLinks extends referenceLink {
 //     id?: number | string,
 // }
-
-
