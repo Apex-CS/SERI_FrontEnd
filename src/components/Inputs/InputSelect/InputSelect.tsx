f9c48955657a3b41c3fd3091329576c438e86d06
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-30 15:55:14 -0600

    Handler InputSelects

--- src/components/Inputs/InputSelect/InputSelect.tsx
+++ src/components/Inputs/InputSelect/InputSelect.tsx
@@ -2,14 +2,16 @@ interface InputSelectProps {
     label: string,
     id: string,
     data: string[],
+	containerClass?: string,
+	value: string,
+	setValue: React.Dispatch<React.SetStateAction<string>>
 }
-function InputSelect({label, data, id = 'inputSelect'} : InputSelectProps) {
-    console.log("🚀 ~ file: InputSelect.tsx:7 ~ InputSelect ~ data:", data)
+function InputSelect({label, data, id = 'inputSelect', containerClass} : InputSelectProps) {
     const defaultClassSelect = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500';
     const defaultClassLabel = 'block mb-2 text-sm font-medium text-gray-900 dark:text-white';
 
 	return (
-		<div>
+		<div className={containerClass}>
 			<label
 				htmlFor={id}
 				className={defaultClassLabel}>
@@ -18,11 +20,9 @@ function InputSelect({label, data, id = 'inputSelect'} : InputSelectProps) {
 			<select
 				id={id}
 				className={defaultClassSelect}>
-				<option selected>Choose a country</option>
-				<option value='US'>United States</option>
-				<option value='CA'>Canada</option>
-				<option value='FR'>France</option>
-				<option value='DE'>Germany</option>
+				{data.map(item => (
+					<option selected>{item}</option>
+				))}
 			</select>
 		</div>
 	);
