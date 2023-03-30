5e074b68b50a9dac020b57b9f0a6a7277aff135f
Diego Alejandro Campanero Negrete <dcampanero@apexsystems.com>
2023-03-30 16:05:33 -0600

    Handlers for InputText

--- src/components/Inputs/InputText/InputText.tsx
+++ src/components/Inputs/InputText/InputText.tsx
@@ -2,28 +2,31 @@ import { InputTextTypeEnum } from "../../../types/generalTypes";
 import { objectIsNull } from "../../../utils/utils";
 
 interface InputTextProps {
-  type?: string;
-  name?: string;
-  label: string;
-  id?: string;
-  placeHolder?: string;
-  required?: boolean;
-  value: string | number,
-  setValue: React.Dispatch<React.SetStateAction<string | number>>,
+	type?: string;
+	name?: string;
+	label?: string;
+	id?: string;
+	placeHolder?: string;
+	required?: boolean;
+	value: string | number;
+	setValue: React.Dispatch<React.SetStateAction<string | number>>;
+	classNameContainer?: string;
+  customClassInput?: string;
 }
 
 function InputText({
-  type = 'text',
-  name = '',
-  label,
-  id = '',
-  placeHolder,
-  required = false,
-  value,
-  setValue
+	type = "text",
+	name = "",
+	label,
+	id = "",
+	placeHolder,
+	required = false,
+	value,
+	setValue,
+	classNameContainer,
+  customClassInput,
 }: InputTextProps) {
-
-  const classLabel = `
+	const classLabel = `
     absolute text-sm text-gray-500 dark:text-gray-400 
     duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0]
     peer-placeholder-shown:scale-100 
@@ -34,40 +37,48 @@ function InputText({
     peer-focus:text-blue-600
     peer-focus:dark:text-blue-500 
     peer-focus:left-0
-    ` 
-  const customClass = `--tw-translate-y: -1.5rem`  
+    `;
+	const customClass = `--tw-translate-y: -1.5rem`;
 
-     // This function is called when the input changes
-  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const enteredValue = event?.target?.value;
-    setValue(enteredValue ? enteredValue : '');
-    
-  };
+  const DEFAULT_CLASS_INPUT = customClassInput ? customClassInput: `
+  block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 
+  border-gray-300 appearance-none dark:text-white dark:border-gray-600 
+  dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer`
 
-  return (
-    <div className="relative z-0 w-full mb-6 group">
-      <input
-        type={type}
-        name={name}
-        value={value}
-        onChange={inputHandler}
-        id={id}
-        className="
-        block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 
-        border-gray-300 appearance-none dark:text-white dark:border-gray-600 
-        dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-        placeholder={placeHolder}
-        required={required}
-      />
-      <label
-        htmlFor={id}
-        style={placeHolder !== undefined  || value !== undefined? { transform: `translateY(-20px)` } : { transform: `translateY(0px)` }  }
-        className={classLabel}
-      >
-        {label}:
-      </label>
-    </div>
-  );
+	// This function is called when the input changes
+	const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const enteredValue = event?.target?.value;
+		setValue(enteredValue ? enteredValue : "");
+	};
+
+	const classContainer = `relative z-0 mb-6 group ${classNameContainer} `;
+
+	return (
+		<div className={classContainer}>
+			<input
+				type={type}
+				name={name}
+				value={value}
+				onChange={inputHandler}
+				id={id}
+				className={DEFAULT_CLASS_INPUT}
+				placeholder={placeHolder}
+				required={required}
+			/>
+			{label && (
+				<label
+					htmlFor={id}
+					style={
+						placeHolder !== undefined || value !== undefined
+							? { transform: `translateY(-20px)` }
+							: { transform: `translateY(0px)` }
+					}
+					className={classLabel}>
+					{label}:
+				</label>
+			)}
+		</div>
+	);
 }
 
 export default InputText;
