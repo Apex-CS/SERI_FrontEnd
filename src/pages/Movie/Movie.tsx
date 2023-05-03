b9ba8b758cc305ffd1d8e9553b81664fc720a37f
diego_c_n <diegoa.campaneron@gmail.com>
2023-05-03 10:19:04 -0600

    advances -- review

--- src/pages/Movie/Movie.tsx
+++ src/pages/Movie/Movie.tsx
@@ -16,7 +16,7 @@ function Movie({}) {
 		const url: string = "";
 		const defaultValueSynopsis =
 			"The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.";
-		const movieElement: Movies = {};
+		const movieElement: Movies = {likes: 0};
 		const [movie, setMovie] = React.useState<Movies>();
 
 	const getStreamChannels = () => {
@@ -59,7 +59,7 @@ function Movie({}) {
 		 */
 		const movie: Movies = {
 			id: getRandomNumber(),
-			poster: "./movies-wallpaper/dr-strangelove.jpg",
+			poster: 'https://m.media-amazon.com/images/M/MV5BZWI3ZTMxNjctMjdlNS00NmUwLWFiM2YtZDUyY2I3N2MxYTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
 			title: "Dr Strangelove",
 			duration: getRandomNumber(240),
 			release_date: new Date(),
@@ -96,25 +96,26 @@ function Movie({}) {
 		<div className='flex w-full flex-col content-center items-center '>
 			<div
 				id='movie-info'
-				className='flex flex-row w-full justify-center items-start content-center px-28 py-5'>
-				{movie ? (
-					<div className='flex flex-row justify-center items-start content-center w-full h-full max-h-max  text-white'>
-						<div className='flex flex-col w-2/4 justify-around items-center'>
-							<div className='flex w-3/4 mb-3 items-start'>
-								<h1 className='text-2xl'>The Goddather</h1>
-							</div>
-							<img
-								className='w-5/12 transition-all duration-300 rounded-lg cursor-pointer filter hover:grayscale'
-								src={imageURL}
-								alt='poster'
-							/>
-						</div>
+				className='flex flex-col w-full justify-center items-start content-center px-28 py-5'>
 
-						<MovieInfo item={movieElement} />
+					<div className='flex w-2/4 mb-3 items-center justify-around'>
+						<h1 className='text-2xl text-white'>{movie?.title}</h1>
 					</div>
-				) : (
-					<Loader />
-				)}
+					
+					{movie ? (
+						<div className='flex flex-row justify-center items-start content-center w-full h-full max-h-max  '>
+							<div className='flex flex-col w-2/4 justify-around items-center'>
+								<img
+									className='object-contain h-3/6 w-5/12 transition-all duration-300 rounded-lg cursor-pointer filter hover:grayscale'
+									src={movie.poster}
+									alt='poster'
+								/>
+							</div>
+							<MovieInfo item={movie} />
+						</div>
+					) : (
+						<Loader />
+					)}
 			</div>
 			<div
 				id='comment-div'
