724bc98b31f428dbc277f07f3ecf1db590953a2c
diego_c_n <diegoa.campaneron@gmail.com>
2023-05-05 15:25:21 -0600

    Update components of MovieItems

--- src/pages/Movies/components/ActionItem.tsx
+++ src/pages/Movies/components/ActionItem.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { DisLike, Eye, Like, Pencil } from "../../../resources/svg/MoviesIcons";
 import { Movies } from "../../../types/types";
+import { LabelSubtitle } from "../../../components";
 
 interface ActionItemProps {
 	item: Movies;
@@ -11,12 +12,12 @@ const ActionItem = ({ item }: ActionItemProps) => {
 	const urlItem = `/movie/movieId:${item.id}`;
 	const editMovieUrl = `/edit-movie/movieId:${item.id}`;
 
-	const classButtons = `text-white m-4 bg-blue-700 h-22
-        hover:bg-blue-800 focus:ring-4 focus:outline-none 
-        focus:ring-blue-300 font-medium rounded-lg text-sm 
+	const classButtons = `text-white m-3 bg-yellow-500 h-22
+        hover:bg-yellow-800 focus:ring-4 focus:outline-none 
+        focus:ring-yellow-300 font-medium rounded-lg text-sm 
         p-2.5 text-center inline-flex items-center mr-2 
-        dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`;
-	const classIconContainer = `flex flex-row m-6`;
+        dark:bg-yellow-500 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800`;
+	const classIconContainer = `flex flex-row mx-6 my-2`;
 
 	const handlerLikeEvent = () => {};
 
@@ -31,28 +32,34 @@ const ActionItem = ({ item }: ActionItemProps) => {
 	};
 
 	return (
-		<div className='flex justify-center flex-col items-center w-40'>
-			<h1 className='text-white'>Vote!</h1>
-			<div className={classIconContainer}>
-				<button className={classButtons}>
-					<Like />
-				</button>
-				<button className={classButtons}>
-					<DisLike />
-				</button>
+		<div className='flex  py-5 justify-start flex-col items-center w-40 mr-10'>
+			<div className="px-5 pb-5">
+				<div className="flex justify-center">
+					<LabelSubtitle customClass="mb-12" subtitle="Vote!" />
+				</div>
+				
+				<div className={classIconContainer}>
+					<button className={classButtons}>
+						<Like />
+					</button>
+					<button className={classButtons}>
+						<DisLike />
+					</button>
+				</div>
+				<div className={`${classIconContainer} mb-2`}>
+					<button
+						onClick={handlerEditEvent}
+						className={classButtons}>
+						<Pencil />
+					</button>
+					<button
+						onClick={handlerViewEvent}
+						className={classButtons}>
+						<Eye />
+					</button>
+				</div>
 			</div>
-			<div className={classIconContainer}>
-				<button
-					onClick={handlerEditEvent}
-					className={classButtons}>
-					<Pencil />
-				</button>
-				<button
-					onClick={handlerViewEvent}
-					className={classButtons}>
-					<Eye />
-				</button>
-			</div>
+			
 		</div>
 	);
 };
--- src/pages/Movies/components/InfoItem.tsx
+++ src/pages/Movies/components/InfoItem.tsx
@@ -1,3 +1,4 @@
+import { LabelSubtitle } from "../../../components";
 import { DisLike, Like } from "../../../resources/svg/MoviesIcons";
 import { Movies } from "../../../types/types";
 
@@ -8,30 +9,28 @@ interface InfoItemProps {
 const InfoItem = ({ item }: InfoItemProps) => (
 	<div
 		id='info-movie'
-		className='flex flex-col justify-between p-5  leading-normal ml-10 mr-10 w-full'>
+		className='flex py-5 flex-col justify-between  leading-normal ml-10 mr-10 w-full'>
 		<div className='flex flex-col justify-start'>
-			<h1 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-				{item.title}
-			</h1>
+			<LabelSubtitle customClass="mb-1" subtitle={item.title ? item.title : ''} />
 			<div>
-				<h5 className='mb-3 font-normal text-gray-700 dark:text-gray-400 text-lg'>
+				<h5 className='mb-3 font-normal text-gray-700 dark:text-gray-400 text-xl'>
 					Duration: {item.duration}
 				</h5>
-				<h5 className='mb-3 font-normal text-gray-700 dark:text-gray-400 text-lg'>
+				<h5 className='mb-3 font-normal text-gray-700 dark:text-gray-400 text-xl'>
 					Clasification: {item.classification}
 				</h5>
-				<h5 className='mb-3 font-normal text-gray-700 dark:text-gray-400 text-lg'>
+				<h5 className='mb-3 font-normal text-gray-700 dark:text-gray-400 text-xl'>
 					Release Date: {item.release_date?.toDateString()}
 				</h5>
 			</div>
 		</div>
 
-		<div className='flex flex-row justify-between items-center lg:w-60 md:w-50 sm:w-40'>
+		<div className='flex flex-row justify-between items-center mb-2 lg:w-60 md:w-50 sm:w-40'>
 			<div className='flex flex-row'>
 				<button
 					type='button'
 					disabled
-					className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+					className='text-white bg-yellow-500 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-yellow-500 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800'>
 					<Like />
 				</button>
 				<h4 className='text-white'>{item.likes}</h4>
@@ -40,7 +39,7 @@ const InfoItem = ({ item }: InfoItemProps) => (
 				<button
 					type='button'
 					disabled
-					className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+					className='text-white bg-yellow-500 hover:bg-yellow-40 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-yellow-500 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800'>
 					<DisLike />
 				</button>
 				<h4 className='text-white'>{item.likes}</h4>
