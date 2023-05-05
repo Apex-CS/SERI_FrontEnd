47f603e70088ff28c872eaab9a0c812f0278205d
diego_c_n <diegoa.campaneron@gmail.com>
2023-05-05 15:26:26 -0600

    update Elements in Movie Details page

--- src/pages/Movie/components/HeaderElement.tsx
+++ src/pages/Movie/components/HeaderElement.tsx
@@ -1,3 +1,4 @@
+import { LabelSubtitle } from "../../../components";
 import { GraphUpArrow, Start, StartFill } from "../../../resources/svg/GeneralIcons";
 import { Movies } from "../../../types/types";
 
@@ -13,26 +14,27 @@ const HeaderElement = ({item}: HeaderElementProps) => {
     return (
         <div className='w-full flex flex-row justify-around items-center mb-2'>
             <div className='flex flex-col justify-between items-center'>
-                <h3>APEX Raiting</h3>
-                <div className="w-3/4 flex flex-row items-center my-1 justify-between">
+                <LabelSubtitle subtitle="APEX Raiting" />
+                <div className="w-3/4 flex flex-row items-center my-1 justify-around">
                     <StartFill />
-                    <h3>9.2/10</h3>
+                    <LabelSubtitle textSize="text-lg" subtitle="9.2/10" />
                 </div>
             </div>
             <div className='flex flex-col justify-center items-center'>
-                <h3>Your Raiting</h3>
+                <LabelSubtitle subtitle="Your Raiting" />
+                
                 <button
                     onClick={handlerOnRateClick}
-                    className="flex items-center justify-between my-1 text-blue-400">
+                    className="flex w-3/4  items-center justify-around my-1 text-blue-400">
                     <Start color="#3B82F6" />
-                    <h1 className="ml-2">Rate</h1>
+                    <h1 className=" text-xl">Rate</h1>
                 </button>
             </div>
             <div className='flex flex-col justify-center items-center'>
-                <h3>Popularity</h3>
-                <div className="w-3/4 flex flex-row items-center my-1 justify-between">
+                <LabelSubtitle subtitle="Popularity"  />
+                <div className="w-3/4 flex flex-row items-center my-1 justify-around">
                     <GraphUpArrow />
-                    <h3>46</h3>
+                    <LabelSubtitle textSize="text-lg" subtitle="46" />
                 </div>
             </div>
         </div>
