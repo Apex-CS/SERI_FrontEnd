9865c8928557d06de82a6b2c4ca8c2614aa60568
diego_c_n <diegoa.campaneron@gmail.com>
2023-05-05 15:23:41 -0600

    fix functions and buttons for pagination component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -7,15 +7,25 @@ const Pagination = ({ length = 6, urlPath }: PaginationProps) => {
 	const DEFAULT_CLASS = 'px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white';
 	const SELECTED_CLASS = 'z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
 
+	const redirectToIndex = (indexPage: number) => {
+		console.log("🚀 ~ file: Pagination.tsx:13 ~ redirectToIndex ~ redirectToIndex: indexPage", indexPage);
+	}
 	
+	const redirectNextPage = () => {
+		console.log("🚀 ~ file: Pagination.tsx:17 ~ redirectNextPage ~ redirectNextPage:");
+	}
+
+	const redirectBeforePage = () => {
+		console.log("🚀 ~ file: Pagination.tsx:22 ~ redirectBeforePage ~ redirectBeforePage :");
+	}
 
 	return (
 		<nav aria-label='Page navigation example'>
 			<ul className='inline-flex items-center -space-x-px'>
 				{/* LEFTH */}
 				<li>
-					<a
-						href='#'
+					<button
+						onClick={redirectBeforePage}
 						className='block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
 						<span className='sr-only'>Previous</span>
 						<svg
@@ -29,23 +39,23 @@ const Pagination = ({ length = 6, urlPath }: PaginationProps) => {
 								d='M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z'
 								clip-rule='evenodd'></path>
 						</svg>
-					</a>
+					</button>
 				</li>
 				{/* NUMBERS */}
 				{Array.from({ length: length }).map((item, index) => (
 					<li>
-						<a
-							href={`${urlPath}+/${index}`}
+						<button
+							onClick={() => redirectToIndex(index)}
 							className={index === 0 ? SELECTED_CLASS : DEFAULT_CLASS}>
 							{index + 1}
-						</a>
+						</button>
 					</li>
 				))}
 
 				{/* RIGHT */}
 				<li>
-					<a
-						href='#'
+					<button
+						onClick={redirectNextPage}
 						className='block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
 						<span className='sr-only'>Next</span>
 						<svg
@@ -59,7 +69,7 @@ const Pagination = ({ length = 6, urlPath }: PaginationProps) => {
 								d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
 								clip-rule='evenodd'></path>
 						</svg>
-					</a>
+					</button>
 				</li>
 			</ul>
 		</nav>
