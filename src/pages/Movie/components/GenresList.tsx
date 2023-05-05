4755087eb8a6cddcc50c582349f4c53d3df2ceed
diego_c_n <diegoa.campaneron@gmail.com>
2023-05-05 15:24:16 -0600

    remove unused code

--- src/pages/Movie/components/GenresList.tsx
+++ src/pages/Movie/components/GenresList.tsx
@@ -4,18 +4,11 @@ interface GenresListProps{
     listItems: GenreCat[],
 }
 function GenresList({listItems}: GenresListProps) {
-    // Probarlo con los Tags
-    // <button
-    //     type='button'
-    //     className='text-white rounded-full font-medium border-2 border-zinc-500 text-sm px-5 py-2 text-center mx-2 hover:bg-zinc-400 active:zinc-400 focus:outline-none'
-    //     >
-    //     {item.description}
-    // </button>
     return (
         <div className="flex flex-row items-center justify-around">
             {
                 listItems.map((item) => (
-                    <p className='text-zinc-200 px-1 py-1'>{item.description},</p>
+                    <p className='text-zinc-200 text-xl px-1 py-1'>{item.description},</p>
             ))}
         </div>
     );
