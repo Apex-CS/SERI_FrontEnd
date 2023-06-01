3af1ddf0d73c39251456d6368708ea8e14f4b691
diego_c_n <diegoa.campaneron@gmail.com>
2023-06-01 15:02:18 -0600

    little fixes and lint code

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,179 +1,168 @@
-type IDElemnt = number | string
+type IDElemnt = number | string;
 
 export interface Comment {
-    id: number;
-    name: string;
-    body: string;
-    reviewedDate: Date,
-    rating: number,
-  }
-  
+  id: number;
+  name: string;
+  body: string;
+  reviewedDate: Date;
+  rating: number;
+}
+
 export interface CommentsState {
-    comments: Comment[];
+  comments: Comment[];
 }
 
 ////// Movies
 export type Movies = {
-    id? : IDElemnt,
-    title? : string,
-    poster? : any, // esto sera una imagen
-    synopsis? : string,
-    originalLanguage? : string,
-    release_date? : Date,
-    duration? : number,
-    likes : number,
-    createdDate? : Date,
-    ModifiedDate? : Date,
-    createdBy? : string,
-    modifiedBy? : string,
-    classification? : string,
+  id: IDElemnt;
+  title: string;
+  poster: any; // esto sera una imagen
+  synopsis: string;
+  originalLanguage: string;
+  release_date: Date;
+  duration: number;
+  likes: number;
+  createdDate: Date;
+  modifiedDate?: Date;
+  createdBy?: string;
+  modifiedBy?: string;
+  classification: string;
 };
 
 export type MoviesGenre = {
-    id: IDElemnt,
-    movieId: number,
-    genreId: number
+  id: IDElemnt;
+  movieId: number;
+  genreId: number;
 };
 
 export type GenreCat = {
-    id: IDElemnt,
-    description: string,
-    selected?: boolean,
+  id: IDElemnt;
+  description: string;
+  selected?: boolean;
 };
 
 export type Tags = {
-    id: IDElemnt,
-    description: string,
-
+  id: IDElemnt;
+  description: string;
 };
 
 export type MoviesTags = {
-    id: IDElemnt,
-    movieId: number,
-    tagId: number,
+  id: IDElemnt;
+  movieId: number;
+  tagId: number;
 };
 
 export type LanguagesCat = {
-    id : IDElemnt,
-    name: string,
+  id: IDElemnt;
+  name: string;
 };
 
 export type Reviews = {
-    id: IDElemnt,
-    moviewId: number,
-    userId: number,
-    review: string,
+  id: IDElemnt;
+  moviewId: number;
+  userId: number;
+  review: string;
 };
 
 export type Rates = {
-    id: IDElemnt,
-    moviewId: number,
-    userId: number,
-    rate: number
+  id: IDElemnt;
+  moviewId: number;
+  userId: number;
+  rate: number;
 };
 
 export type MoviesCollaborators = {
-    id: IDElemnt,
-    moviewId: number,
-    collaboratorId: number,
+  id: IDElemnt;
+  moviewId: number;
+  collaboratorId: number;
 };
 
 export type MoviesHostedOn = {
-    id: IDElemnt,
-    moviewId: number,
-    stramingPlatformId: number,
+  id: IDElemnt;
+  moviewId: number;
+  stramingPlatformId: number;
 };
 
 export type ClassificationCat = {
-    id: IDElemnt,
-    description: string,
+  id: IDElemnt;
+  description: string;
 };
 
-
-
 export type MoviesClasification = {
-    id: IDElemnt,
-    moviewId: number,
-    clasificartionId: number,
+  id: IDElemnt;
+  moviewId: number;
+  clasificartionId: number;
 };
 
 export type Collaborators = {
-    id: IDElemnt,
-
+  id: IDElemnt;
 };
 
 export type RolesCat = {
-    id: IDElemnt,
-
+  id: IDElemnt;
 };
 
 export type StreamingPlatformsCat = {
-    id: IDElemnt,
-    name: string,
-    logo: string, // Si no tambien hacerlo con Any
-    select?: boolean,
-    url?: string,
+  id: IDElemnt;
+  name: string;
+  logo: string; // Si no tambien hacerlo con Any
+  select?: boolean;
+  url?: string;
 };
 
 export type Director = {
-    id: IDElemnt,
-    name: string,
-}
+  id: IDElemnt;
+  name: string;
+};
 
 export type Star = {
-    id: IDElemnt,
-    name: string,
-}
+  id: IDElemnt;
+  name: string;
+};
 
 export type Writer = {
-    id: IDElemnt,
-    name: string,
-}
-
+  id: IDElemnt;
+  name: string;
+};
 
 //////7 USERS
 
 export type Users = {
-    id: IDElemnt,
-
+  id: IDElemnt;
 };
 
 export type UsersRol = {
-    id: IDElemnt,
-
+  id: IDElemnt;
 };
 
 export enum ClassificationCatEnum {
-    A = 'A',
-    AA = 'AA',
-    B = 'B',
-    B15 = 'B-15',
-    C = 'C',
-    NA = 'N/A',
-    D = 'D',
+  A = "A",
+  AA = "AA",
+  B = "B",
+  B15 = "B-15",
+  C = "C",
+  NA = "N/A",
+  D = "D",
 }
 
 export enum LanguageEnum {
-    English= "English",
-    Mandarin= "Mandarin",
-    French= "French",
-    Japanese= "Japanese",
-    Spanish= "Spanish",
-    Korean= "Korean",
-    German= "German",
-    Hindi= "Hindi",
-    Cantonese= "Cantonese",
-    Italian= "Italian",
-    Russian= "Russian",
-    Swedish= "Swedish",
-    Arabic= "Arabic",
-    Navi= "Na'vi",
-    Latin= "Latin",
-    Portuguese= "Portuguese",
-    Hebrew= "Hebrew",
-    Turkish= "Turkish",
-    Polish= "Polish",
+  English = "English",
+  Mandarin = "Mandarin",
+  French = "French",
+  Japanese = "Japanese",
+  Spanish = "Spanish",
+  Korean = "Korean",
+  German = "German",
+  Hindi = "Hindi",
+  Cantonese = "Cantonese",
+  Italian = "Italian",
+  Russian = "Russian",
+  Swedish = "Swedish",
+  Arabic = "Arabic",
+  Navi = "Na'vi",
+  Latin = "Latin",
+  Portuguese = "Portuguese",
+  Hebrew = "Hebrew",
+  Turkish = "Turkish",
+  Polish = "Polish",
 }
-
-
-
