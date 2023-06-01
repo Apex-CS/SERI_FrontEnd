c818eeac6fb03cf4820e8d2a751c1dc61ec01263
diego_c_n <diegoa.campaneron@gmail.com>
2023-06-01 15:01:32 -0600

    Fix Movie Page

--- src/pages/Movie/Movie.tsx
+++ src/pages/Movie/Movie.tsx
@@ -28,7 +28,7 @@ function Movie({}) {
   const url: string = "";
   const defaultValueSynopsis =
     "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.";
-  const movieElement: Movies = { likes: 0 };
+  // const movieElement: Movies = {} as Movies;
   const [movie, setMovie] = React.useState<Movies>();
 
   const getStreamChannels = () => {
@@ -80,7 +80,7 @@ function Movie({}) {
       synopsis: `El general de la Fuerza Aérea de los Estados Unidos, Jack D. Ripper (Sterling Hayden; nombre que se pronuncia Jack The Ripper, Jack el Destripador), planea dar comienzo a una guerra nuclear con la Unión Soviética con el objetivo de impedir lo que considera una conspiración comunista para fluorizar el agua, contaminando así los "preciosos fluidos corporales" de los estadounidenses. Da la orden, sin la autorización del presidente Merkin Muffley (Peter Sellers), a su escuadra nuclear de combate, de bombardear sus respectivos objetivos dentro de la Unión Soviética, con la esperanza de que el presidente ordene un ataque a gran escala al no encontrar otra opción.
 			El general Ripper desconoce, sin embargo, que los soviéticos cuentan con un "Dispositivo del Fin del Mundo", el cual se activaría automáticamente en caso de detectar un ataque nuclear sobre territorio de la Unión Soviética, destruyendo toda vida sobre la faz de la Tierra por contaminación radiactiva.`,
       classification: getRandomClassification(),
-    };
+    } as Movies;
     return movie;
   };
 
